f9407cda2785e2634bcde3bd774f2e38624424d9
Gabe <gabe.galperin@gmail.com>
2012-12-11 18:44:00 -0500

    Follow extjs loader standards
    Library can now be loaded with extjs loader, and compiled with Sencha
    Cmd3

--- src/BoxSelect.js
+++ src/BoxSelect.js
@@ -29,9 +29,10 @@
  * @xtype boxselect
  *
  */
-Ext.define('Ext.ux.form.field.BoxSelect', {
+Ext.define('Ext.ux.boxselect.BoxSelect', {
     extend:'Ext.form.field.ComboBox',
     alias: ['widget.comboboxselect', 'widget.boxselect'],
+    uses: ['Ext.ux.boxselect.BoxSelectField'],
     requires: ['Ext.selection.Model', 'Ext.data.Store'],
 
     //
@@ -1590,72 +1591,5 @@ Ext.define('Ext.ux.form.field.BoxSelect', {
                 delete me.autoSizing;
             }
         }
-    }
-});
-
-/**
- * Ensures the input element takes up the maximum amount of remaining list width,
- * or the entirety of the list width if too little space remains. In this case,
- * the list height will be automatically increased to accomodate the new line. This
- * growth will not occur if {@link Ext.ux.form.field.BoxSelect#multiSelect} or
- * {@link Ext.ux.form.field.BoxSelect#grow} is false.
- */
-Ext.define('Ext.ux.layout.component.field.BoxSelectField', {
-    /* Begin Definitions */
-    alias: ['layout.boxselectfield'],
-    extend: 'Ext.layout.component.field.Trigger',
-
-    /* End Definitions */
-
-    type: 'boxselectfield',
-
-    /*For proper calculations we need our field to be sized.*/
-    waitForOuterWidthInDom:true,
-
-    beginLayout: function(ownerContext) {
-        var me = this,
-            owner = me.owner;
-
-        me.callParent(arguments);
-
-        ownerContext.inputElCtContext = ownerContext.getEl('inputElCt');
-        owner.inputElCt.setStyle('width','');
-
-        me.skipInputGrowth = !owner.grow || !owner.multiSelect;
-    },
-
-    beginLayoutFixed: function(ownerContext, width, suffix) {
-        var me = this,
-            owner = ownerContext.target;
-
-        owner.triggerEl.setStyle('height', '24px');
-
-        me.callParent(arguments);
-
-        if (ownerContext.heightModel.fixed && ownerContext.lastBox) {
-            owner.listWrapper.setStyle('height', ownerContext.lastBox.height+'px');
-            owner.itemList.setStyle('height', '100%');
-        }
-        /*No inputElCt calculations here!*/
-    },
-
-    /*Calculate and cache value of input container.*/
-    publishInnerWidth:function(ownerContext) {
-        var me = this,
-            owner = me.owner,
-            width = owner.itemList.getWidth(true) - 10,
-            lastEntry = owner.inputElCt.prev(null, true);
-
-        if (lastEntry && !owner.stacked) {
-            lastEntry = Ext.fly(lastEntry);
-            width = width - lastEntry.getOffsetsTo(lastEntry.up(''))[0] - lastEntry.getWidth();
-        }
-
-        if (!me.skipInputGrowth && (width < 35)) {
-            width = width - 10;
-        } else if (width < 1) {
-            width = 1;
-        }
-        ownerContext.inputElCtContext.setWidth(width);
     }
 });
